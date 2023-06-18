996783681734effc39e9f0d44c159767276939dc
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2023-06-18 15:29:08 +0800

    Automated build.

--- src/cran_pkg.sh
+++ src/cran_pkg.sh
@@ -3,70 +3,76 @@
 set -e
 
 ## tidyverse
-R -e "             \
-  pkg <- c(        \
-    'tidyverse',   \
-    'devtools',    \
-    'rmarkdown',   \
-    'BiocManager', \
-    'vroom',       \
-    'gert'         \
-  );               \
-  for (i in seq_along(pkg)) if (!require(pkg[[i]])) install.packages(pkg[[i]], repos = 'https://packagemanager.rstudio.com/all/latest') \
+R -e "               \
+  install.packages(  \
+    c(               \
+      'tidyverse',   \
+      'devtools',    \
+      'rmarkdown',   \
+      'BiocManager', \
+      'vroom',       \
+      'gert'         \
+    ),               \
+    repos = 'https://packagemanager.rstudio.com/all/latest' \
+  ) \
 "
 
 ## dplyr database backends
-R -e "              \
-  pkg <- c(         \
-    'arrow',        \
-    'dbplyr',       \
-    'DBI',          \
-    'dtplyr',       \
-    'duckdb',       \
-    'nycflights13', \
-    'Lahman',       \
-    'RMariaDB',     \
-    'RPostgres',    \
-    'RSQLite',      \
-    'fst'          \
-  );               \
-  for (i in seq_along(pkg)) if (!require(pkg[[i]])) install.packages(pkg[[i]], repos = 'https://packagemanager.rstudio.com/all/latest') \
+R -e "                \
+  install.packages(   \
+    c(                \
+      'arrow',        \
+      'dbplyr',       \
+      'DBI',          \
+      'dtplyr',       \
+      'duckdb',       \
+      'nycflights13', \
+      'Lahman',       \
+      'RMariaDB',     \
+      'RPostgres',    \
+      'RSQLite',      \
+      'fst'           \
+    ),                \
+    repos = 'https://packagemanager.rstudio.com/all/latest' \
+  ) \
 "
 
 ## a bridge to far? -- brings in another 60 packages
-R -e "           \
-  pkg <- c(      \
-    'tidymodels' \
-  );             \
-  for (i in seq_along(pkg)) if (!require(pkg[[i]])) install.packages(pkg[[i]], repos = 'https://packagemanager.rstudio.com/all/latest') \
+R -e "                                                      \
+  install.packages(                                         \
+    'tidymodels',                                           \
+    repos = 'https://packagemanager.rstudio.com/all/latest' \
+  )                                                         \
 "
 
 R -e "                \
-  pkg <- c(           \
-    'covr',           \
-    'devtools',       \
-    'distro',         \
-    'ggplot2',        \
-    'knitr',          \
-    'languageserver', \
-    'lintr',          \
-    'magick',         \
-    'microbenchmark', \
-    'pdftools',       \
-    'pkgdown',        \
-    'ragg',           \
-    'remotes',        \
-    'rmarkdown',      \
-    'rprojroot',      \
-    'styler',         \
-    'testthat',       \
-    'tidyverse',      \
-    'qpdf',           \
-    'semmcci',        \
-    'betaDelta',      \
-    'betaSandwich',   \
-    'betaNB',         \
-    'betaMC'          \
-  );                  \
-  for (i in seq_along(pkg)) if (!require(pkg[[i]])) install.packages(pkg[[i]], repos = 'https://packagemanager.rstudio.com/all/latest') \                
+  install.packages(   \
+    c(                \
+      'covr',           \
+      'devtools',       \
+      'distro',         \
+      'ggplot2',        \
+      'knitr',          \
+      'languageserver', \
+      'lintr',          \
+      'magick',         \
+      'microbenchmark', \
+      'pdftools',       \
+      'pkgdown',        \
+      'ragg',           \
+      'remotes',        \
+      'rmarkdown',      \
+      'rprojroot',      \
+      'styler',         \
+      'testthat',       \
+      'tidyverse',      \
+      'qpdf',           \
+      'semmcci',        \
+      'betaDelta',      \
+      'betaSandwich',   \
+      'betaNB',         \
+      'betaMC'          \
+    ),                \
+    repos = 'https://packagemanager.rstudio.com/all/latest' \
+  ) \
 "
