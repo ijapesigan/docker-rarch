ff389d6f43accfe98a4e3c775851801068afe1f8
ijapesigan <ijapesigan@gmail.com>
2024-01-17 20:05:12 +0000

    Automated build.

--- scripts/github_pkg.sh
+++ scripts/github_pkg.sh
@@ -2,18 +2,19 @@
 
 set -e
 
-R -e "remotes::install_github(      \
-    c(                              \
-        'rstudio/tinytex',          \
-        'r-lib/cli',                \
-        'r-lib/devtools',           \
-        'r-hub/rhub',               \
-        'ijapesigan/rProject',      \
-        'jeksterslab/semmcci',      \
-        'jeksterslab/betaDelta',    \
-        'jeksterslab/betaSandwich', \
-        'jeksterslab/betaNB',       \
-        'jeksterslab/betaMC',       \
-        'jeksterslab/simStateSpace' \
-    )                               \
+R -e "remotes::install_github(       \
+    c(                               \
+        'rstudio/tinytex',           \
+        'r-lib/cli',                 \
+        'r-lib/devtools',            \
+        'r-hub/rhub',                \
+        'ijapesigan/rProject',       \
+        'jeksterslab/semmcci',       \
+        'jeksterslab/betaDelta',     \
+        'jeksterslab/betaSandwich',  \
+        'jeksterslab/betaNB',        \
+        'jeksterslab/betaMC',        \
+        'jeksterslab/simStateSpace', \
+        'ijapesigan/fitOU'           \
+    )                                \
 )"
