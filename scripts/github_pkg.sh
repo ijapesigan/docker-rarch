a151af2d0fd2b6287b3b07471db9ce3bbc0c7d43
ijapesigan <ijapesigan@gmail.com>
2025-06-29 19:43:20 +0000

    Automated build.

--- scripts/github_pkg.sh
+++ scripts/github_pkg.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-R -e "remotes::install_github(       \
-    c(                               \
-        'rstudio/tinytex',           \
-        'r-lib/cli',                 \
-        'r-lib/devtools',            \
-        'ijapesigan/rProject',       \
-        'jeksterslab/semmcci',       \
-        'jeksterslab/betaDelta',     \
-        'jeksterslab/betaSandwich',  \
-        'jeksterslab/betaNB',        \
-        'jeksterslab/betaMC',        \
-        'jeksterslab/simStateSpace', \
-        'jeksterslab/cTMed',         \
-        'jeksterslab/fitDTVARMx',    \
-        'jeksterslab/fitCTVARMx',    \
-        'jeksterslab/metaVAR'        \
-    )                                \
+R -e "remotes::install_github(        \
+    c(                                \
+        'rstudio/tinytex',            \
+        'r-lib/cli',                  \
+        'r-lib/devtools',             \
+        'ijapesigan/rProject',        \
+        'jeksterslab/semmcci',        \
+        'jeksterslab/betaDelta',      \
+        'jeksterslab/betaSandwich',   \
+        'jeksterslab/betaNB',         \
+        'jeksterslab/betaMC',         \
+        'jeksterslab/simStateSpace',  \
+        'jeksterslab/bootStateSpace', \
+        'jeksterslab/cTMed',          \
+        'jeksterslab/fitDTVARMx',     \
+        'jeksterslab/metaVAR'         \
+    )                                 \
 )"
